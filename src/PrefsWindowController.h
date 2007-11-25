7a40e48e9829bb59bb52baf25b640af1594f0600
David Symonds <dsymonds@gmail.com>
2007-11-25 15:07:19 +1100

    Hook up trigger removal button.

--- src/PrefsWindowController.h
+++ src/PrefsWindowController.h
@@ -4,6 +4,7 @@
 #import "ContextTree.h"
 #import "ContextSelectionButton.h"
 #import "MPController.h"
+#import "PopButton.h"
 
 @interface PrefsWindowController : NSWindowController
 {
@@ -20,9 +21,8 @@
 	IBOutlet NSArrayController *rulesController, *filteredRulesController, *actionsController;
 	IBOutlet NSArrayController *whenActionController;
 
-	// Selection controls for rules/actions
+	// Selection controls for rules
 	IBOutlet ContextSelectionButton *defaultContextButton;
-	IBOutlet ContextSelectionButton *editActionContextButton;
 
 	// New action creation hooks
 	IBOutlet NSWindow *newActionWindow;
@@ -33,6 +33,9 @@
 	IBOutlet NSArrayController *newActionLimitedOptionsController;
 	IBOutlet NSPopUpButton *newActionContext;
 	NSString *newActionWindowWhen;
+
+	// Action trigger hooks
+	IBOutlet PopButton *newActionTriggerButton;
 
 	IBOutlet NSTextView *logBufferView;
 	NSNumber *logBufferPaused;
