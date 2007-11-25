fe8462ed70ba9a2681766f3d0b59995736f66f3f
David Symonds <dsymonds@gmail.com>
2007-11-26 00:48:12 +1100

    Monitor RSIZE as well as VSIZE in vsize-monitor.sh script.

--- scripts/vsize-monitor.sh
+++ scripts/vsize-monitor.sh
@@ -7,15 +7,21 @@ SLEEP_INTERVAL=10
 
 last_vsize=-1
 while true ; do
-	vsize=$(ps -p $MP_PID -o vsize | tail -1)
+	rsize=$(ps -p $MP_PID -o rsz | tail -1 | tr -d ' ')
+	vsize=$(ps -p $MP_PID -o vsize | tail -1 | tr -d ' ')
+	printf "RSIZE/VSIZE: $rsize/$vsize KB"
+
 	if [ $last_vsize -eq -1 ]; then
-		echo "VSIZE: $vsize KB"
+		echo
 	else
-		gain=$(($vsize - $last_vsize))
-		rate=$(($gain / $SLEEP_INTERVAL))
-		echo "VSIZE: $vsize KB (gain: $gain == $rate/s)"
+		r_gain=$(($rsize - $last_rsize))
+		r_rate=$(($r_gain / $SLEEP_INTERVAL))
+		v_gain=$(($vsize - $last_vsize))
+		v_rate=$(($v_gain / $SLEEP_INTERVAL))
+		echo " (gain: $r_gain/$v_gain ==> $r_rate/$v_rate KB/s)"
 	fi
 
+	last_rsize=$rsize
 	last_vsize=$vsize
 	sleep $SLEEP_INTERVAL
 done
