79a30b15dc18b4d97010b6130528379ee3f592a7
David Symonds <dsymonds@gmail.com>
2007-11-27 16:28:56 +1100

    Implement some variable substitution in ShellScript action (Trac #152).
    The following substitutions are implemented:
    	%context.name% - Name of current context
    	%context.path% - Full path for current context

--- src/ShellScriptAction.m
+++ src/ShellScriptAction.m
@@ -5,8 +5,18 @@
 //  Created by David Symonds on 23/04/07.
 //
 
+#import "ContextTree.h"
+#import "MPApplication.h"
 #import "ShellScriptAction.h"
 
+
+@interface ShellScriptAction (Private)
+
+- (NSMutableArray *)splitPathAfterSubstitutions;
+
+@end
+
+#pragma mark -
 
 @implementation ShellScriptAction
 
@@ -48,13 +58,40 @@
 
 - (NSString *)description
 {
-	return [NSString stringWithFormat:NSLocalizedString(@"Running shell script '%@'.", @""), path];
+	return [NSString stringWithFormat:NSLocalizedString(@"Running shell script '%@'.", @""),
+		[[self splitPathAfterSubstitutions] objectAtIndex:0]];
+}
+
+- (NSMutableArray *)splitPathAfterSubstitutions
+{
+	Context *context = [NSApp currentContext];
+	NSString *contextPath = [[ContextTree sharedInstance] pathFromRootTo:[context uuid]];
+
+	NSMutableArray *res = [NSMutableArray array];
+	NSEnumerator *en = [[path componentsSeparatedByString:@"|"] objectEnumerator];
+	NSString *arg;
+	while ((arg = [en nextObject])) {
+		NSMutableString *str = [[arg mutableCopy] autorelease];
+
+		[str replaceOccurrencesOfString:@"%context.name%"
+				     withString:[context name]
+					options:0
+					  range:NSMakeRange(0, [str length])];
+		[str replaceOccurrencesOfString:@"%context.path%"
+				     withString:contextPath
+					options:0
+					  range:NSMakeRange(0, [str length])];
+
+		[res addObject:str];
+	}
+
+	return res;
 }
 
 - (BOOL)execute:(NSString **)errorString
 {
 	// Split on "|", add "--" to the start so that the shell won't try to parse arguments
-	NSMutableArray *args = [[[path componentsSeparatedByString:@"|"] mutableCopy] autorelease];
+	NSMutableArray *args = [self splitPathAfterSubstitutions];
 	[args insertObject:@"--" atIndex:0];
 
 	NSTask *task = [NSTask launchedTaskWithLaunchPath:@"/bin/sh" arguments:args];
