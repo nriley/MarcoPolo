22d2b847e687bdda6421a4110ef99289366b646a
David Symonds <dsymonds@gmail.com>
2007-12-06 16:03:46 +1100

    Implement a working TimeZone action.
    The menubar clock stays incorrect, but other things seem to respond fine.

--- src/TimeZoneAction.m
+++ src/TimeZoneAction.m
@@ -26,35 +26,50 @@
 
 - (BOOL)execute:(NSDictionary *)actionDict error:(NSString **)errorString
 {
-	// TODO: verify things more carefully
-
 	NSString *zone = [actionDict valueForKey:@"parameter"];
+
+	// Sanity check: parameter should describe a known timezone
 	NSTimeZone *tz = [NSTimeZone timeZoneWithName:zone];
 	if (!tz) {
+		NSLog(@"Didn't recognise '%@' as a time zone", zone);
 		*errorString = NSLocalizedString(@"Couldn't set time zone!", @"");
 		return NO;
 	}
 
-	// TODO: Relink /etc/localtime symlink to /usr/share/zoneinfo/<zone-name>,
-	// where <zone-name> is like Australia/Sydney
-
-	// TODO: Would we need to restart any standard system programs to get clock displays correct?
-
-//	NSArray *args = [NSArray arrayWithObjects:@"-d", printerQueue, nil];
-//	NSTask *task = [NSTask launchedTaskWithLaunchPath:@"/usr/bin/lpoptions" arguments:args];
-//	[task waitUntilExit];
-//
-//	if ([task terminationStatus] != 0) {
+	// Sanity check: parameter should have no more than one "/" in it, and no periods
+	if ([[zone componentsSeparatedByString:@"/"] count] > 2) {
+		NSLog(@"'%@' has too many slashes", zone);
 		*errorString = NSLocalizedString(@"Couldn't set time zone!", @"");
 		return NO;
-//	}
-//
-//	return YES;
+	} else if ([[zone componentsSeparatedByString:@"."] count] > 1) {
+		NSLog(@"'%@' has too many periods", zone);
+		*errorString = NSLocalizedString(@"Couldn't set time zone!", @"");
+		return NO;
+	}
+
+	// TODO: verify things more carefully?
+
+	// Relink /etc/localtime symlink to /usr/share/zoneinfo/<zone-name>,
+	// where <zone-name> is like Australia/Sydney
+	NSString *destPath = [@"/usr/share/zoneinfo" stringByAppendingPathComponent:zone];
+	NSString *tool = @"/bin/ln";
+	NSArray *args = [NSArray arrayWithObjects:@"-sfv", destPath, @"/etc/localtime", nil];
+	NSString *prompt = NSLocalizedString(@"MarcoPolo needs to change a system file to set your timezone.\n\n",
+					     @"In TimeZoneAction");
+
+	if (![self authExec:tool args:args authPrompt:prompt]) {
+		*errorString = NSLocalizedString(@"Couldn't set time zone!", @"");
+		return NO;
+	}
+
+	// TODO: What standard system programs do we need to restart to get clock displays correct?
+
+	return YES;
 }
 
 - (NSString *)suggestionLeadText
 {
-	return NSLocalizedString(@"Use this time zone:", @"");
+	return NSLocalizedString(@"Set this time zone:", @"");
 }
 
 - (NSArray *)suggestions
@@ -69,7 +84,7 @@
 		NSString *desc = [NSString stringWithFormat:@"%@ (%@) (%+04d)",
 				[zone name], [zone abbreviation], [zone secondsFromGMT] / 36];
 		[opts addObject:[NSDictionary dictionaryWithObjectsAndKeys:
-				[zone name], @"option", desc, @"description", nil]];
+				[zone name], @"parameter", desc, @"description", nil]];
 	}
 
 	return opts;
